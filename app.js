7e928f81d98542483b0312c5ca3404bf80c0dae5
Daniil <www.daniiland@gmail.com>
2023-10-09 19:17:13 +0800

    Логика с меню в отдельном файле

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-var menu = document.querySelector('.menu-wrapper')
-var textMenu = document.querySelector('header > .menu > .burger-menu')
-var burgerMenu = document.querySelector('.burger-menu')
-
-var menuOpen = false
-
-burgerMenu.addEventListener('mousedown', function () {
-  if (!menuOpen) {
-    burgerMenu.classList.add('open')
-    textMenu.classList.add('open')
-    menu.classList.add('open')
-    menuOpen = true
-  } else {
-    burgerMenu.classList.remove('open')
-    textMenu.classList.remove('open')
-    menu.classList.remove('open')
-    menuOpen = false
-  }
-})
-
 let choiceDevice = ''
 let choiceObject = ''
 let choiceScenario = ''
