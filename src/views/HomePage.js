9c82708ebf8ef7e42e5d43734501e5634e3a019e
Daniil <www.daniiland@gmail.com>
2023-10-11 21:52:31 +0800

    Проверка подсветки синтаксиса html в js

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,13 +1,13 @@
 function renderHomePage() {
-  content.innerHTML = `
-  <h1 class="hero-text">Будущее за технологиями.</h1>
+  content.innerHTML = String.raw`
+    <h1 class="hero-text">Будущее за технологиями.</h1>
 
-  <h2 class="info-text">
-    Начните использовать возможности передовых устройств для оптимизации производственного
-    процесса
-  </h2>
+    <h2 class="info-text">
+      Начните использовать возможности передовых устройств для оптимизации производственного
+      процесса
+    </h2>
 
-  <button id="btn-do-work" onclick='renderSelectingWorkScenarioPage()'>Начать работу</button>
+    <button id="btn-do-work" onclick="renderSelectingWorkScenarioPage()">Начать работу</button>
   `
   setStyleCSS()
 }
