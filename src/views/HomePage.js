c2d62e4fc3a421f3a8e7d0d9087de9572d0735ae
Daniil <www.daniiland@gmail.com>
2023-10-15 22:08:27 +0800

    Исправлено расположение текста

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -2,14 +2,24 @@ function renderHomePage() {
   content.innerHTML = /*html*/ `
     <h1 class="hero-text">Будущее за технологиями</h1>
 
-    <h2 class="info-text">
-      Начните использовать возможности передовых устройств для&nbsp;оптимизации производственного
-      процесса
-    </h2>
+    <div class="hero__iner">
+      <h2 class="info-text">
+        Начните использовать возможности передовых устройств для&nbsp;оптимизации производственного
+        процесса
+      </h2>
 
-    <button id="btn-do-work" onclick="checkUserAuthAndRedirect()">Начать работу</button>
+      <button id="btn-do-work" onclick="checkUserAuthAndRedirect()">Начать работу</button>
+    </div>
   `
-  setStyleCSS()
+  setStyleCSS(/*css*/ `
+    .hero__iner {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      height: 100%;
+      overflow-y: auto;
+    }
+  `)
 }
 
 function checkUserAuthAndRedirect() {
