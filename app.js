92d66f286be63acf87c36a9e265a2545b4944bc1
Daniil <www.daniiland@gmail.com>
2023-10-16 12:19:48 +0800

    Добавлены иконки

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ function setStyleCSS(styles) {
 
 function setUserAuth() {
   isUserAuth = confirm('Авторизоваться?')
+  if (isUserAuth) {
+    document.getElementById('img_user').setAttribute('src', 'src/img/user-logout.svg')
+  } else {
+    document.getElementById('img_user').setAttribute('src', 'src/img/user.svg')
+  }
 }
 
 renderHomePage()
