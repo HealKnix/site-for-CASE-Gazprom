d3261b442365a43443c3e5795fdb89caa07a3c6b
Daniil <www.daniiland@gmail.com>
2023-10-17 19:43:17 +0800

    Изменена ширина обводки

--- src/views/ControlDevicePage.js
+++ src/views/ControlDevicePage.js
@@ -108,9 +108,9 @@ function renderControlDevicePage(currentDevice) {
         <div class="card">
           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-wind" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z"/>
-            <path stroke="#0fa966" d="M5 8h8.5a2.5 2.5 0 1 0 -2.34 -3.24"/>
-            <path stroke="#0fa966" d="M3 12h15.5a2.5 2.5 0 1 1 -2.34 3.24"/>
-            <path stroke="#0fa966" d="M4 16h5.5a2.5 2.5 0 1 1 -2.34 3.24"/>
+            <path stroke="#0fa966" stroke-width="1" d="M5 8h8.5a2.5 2.5 0 1 0 -2.34 -3.24"/>
+            <path stroke="#0fa966" stroke-width="1" d="M3 12h15.5a2.5 2.5 0 1 1 -2.34 3.24"/>
+            <path stroke="#0fa966" stroke-width="1" d="M4 16h5.5a2.5 2.5 0 1 1 -2.34 3.24"/>
           </svg>
           <span>Датчик ветра</span>
         </div>
